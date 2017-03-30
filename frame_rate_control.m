52a49b2f591d1186d46c33405741cc786c0b8bdc
Jaewon Choi <jaewon.james.choi@gmail.com>
2017-03-30 21:37:26 +0900

    Initial commit

---
+++ frame_rate_control.m
@@ -0,0 +1,26 @@
+x = [60 30 15];
+y1 = [5.83668027 5.534939044 5.176608811];
+y2 = [4.645700278 4.049172509 4.049172509];
+y3 = [4.645700278 4.049172509 4.049172509];
+
+figure
+p = plot(x,y1, '-bx',...
+    x,y2, '-go',...
+    x,y3, '-rs',...
+    'LineWidth', 2,...
+    'MarkerSize', 10);
+
+title('Energy consumption', 'fontsize', 18);
+xlabel('frame rate (frame per second)', 'fontsize', 18);
+ylabel('elapsed time (hour)', 'fontsize', 18);
+legend({'1 bunny', '10 bunnies', '100 buunies'}, 'FontSize', 18);
+axis([10 65 3.0 6.5]);
+
+axis_data = gca;
+axis_data.YAxis.TickLabelFormat = '%,.1f';
+axis_data.XTick = [15 30 60];
+set(gca,'xgrid','on')
+
+dim = [.2 .5 .3 .3];
+str = 'A';
+annotation('textbox', dim,'String',str,'FitBoxToText','on');
